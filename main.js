e6f522b9c2f0507dd83c61dc74ab03171a22def6
Milos1988 <vojvoda1988@gmail.com>
2023-01-13 12:17:44 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const arrowRight = document.querySelector(".arrowRight");
+const arrowLeft = document.querySelector(".arrowLeft");
+const mainImg = document.querySelector(".container img");
+console.log(mainImg)
+
+let numImg = 5;
+let activeSlider = 1;
+
+arrowRight.addEventListener("click", () => {
+    activeSlider++
+    if (activeSlider <= numImg) {
+        document.body.style.backgroundImage = `url(img/img${activeSlider}.jpg)`;
+        mainImg.setAttribute("src", `img/img${activeSlider}.jpg`)
+
+    } else {
+        activeSlider = 1;
+        document.body.style.backgroundImage = `url(img/img${activeSlider}.jpg)`;
+        mainImg.setAttribute("src", `img/img${activeSlider}.jpg`)
+        // activeSlider++
+    }
+});
+
+arrowLeft.addEventListener("click", () => {
+    activeSlider--
+    if (activeSlider <= 0) {
+        activeSlider = numImg;
+        document.body.style.backgroundImage = `url(img/img${activeSlider}.jpg)`;
+        mainImg.setAttribute("src", `img/img${activeSlider}.jpg`)
+
+    } else {
+        // activeSlider = numImg;
+        document.body.style.backgroundImage = `url(img/img${activeSlider}.jpg)`;
+        mainImg.setAttribute("src", `img/img${activeSlider}.jpg`)
+
+    }
+})
